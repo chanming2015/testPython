17fb14b499411d1019c3bf8a349f498e217c21e7
maosen.xu <maosen.xu@aispeech.com>
2022-06-02 11:42:55 +0800

    .

---
+++ aispeech/ddsserver/dm/sync_skill_conf.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Created on 2019年3月2日
+
+@author: XuMaosen
+'''
+
+import requests
+import json
+import time
+       
+if __name__ == '__main__':
+    productIds = ['tryProductId', '279609734', '279606354', '278582558', '279610639', '279610836']
+    skillIds = ['2022011900000133']
+    j = json.loads(open(u'C:/Users/Administrator/Desktop/长城车控技能配置.json', encoding='utf-8').read())
+    for p in productIds:
+        for s in skillIds:
+            url = '?productId=%s&skillId=%s' % (p, s)
+            resp = requests.post(url, json=j)
+            print(resp.status_code)
+            time.sleep(2)
