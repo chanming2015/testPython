656dcd9d0462f70ec2e1ba36efdec7702dab7e36
maosen.xu <maosen.xu@aispeech.com>
2023-04-06 20:51:59 +0800

    bug fix.

--- aispeech/ddsserver/dm/autotest/car_ctl_dm_test_reality.py
+++ aispeech/ddsserver/dm/autotest/car_ctl_dm_test_reality.py
@@ -8,6 +8,7 @@ Created on 2023-03-25
 """
 import asyncio
 import json
+import time
 from uuid import uuid4
 
 import pandas as pd
@@ -16,11 +17,11 @@ import websockets
 # 设置产品ID
 PRODUCT_ID = "279606354"
 # 设置分支
-BRANCH = "prod_p11"
+BRANCH = "car_ctl_beta_test"
 # 设置授权APIKEY；DUI开放平台-授权管理
 APIKEY = ""
 # True：单轮测试；False：多轮测试
-is_single_round_test = True
+is_single_round_test = False
 
 # 必填参数非空校验
 if PRODUCT_ID == "" or BRANCH == "" or APIKEY == "":
@@ -30,31 +31,36 @@ if PRODUCT_ID == "" or BRANCH == "" or APIKEY == "":
 # 定义测试服务地址
 SERVER_URL = "wss://dds.dui.ai/dds/v3/%s?serviceType=websocket&productId=%s&apikey=%s&deviceName=carCtlDmTestAutoTest&communicationType=fullDuplex" % (BRANCH, PRODUCT_ID, APIKEY)
 # 读取Excel文件测试数据
-file_name = "车控测试集2.xlsx"
-sheet_name = "单轮车控测试集"
+file_name = "多轮车控测试集.xlsx"
+sheet_name = "多轮车控测试集"
 lines = pd.read_excel(file_name, sheet_name=sheet_name, header=None).values.tolist()
 file_head = lines[0]
 
 # 定义测试数据列索引
 index_refText = file_head.index("测试用例")
-index_skill = file_head.index("技能")
-index_task = file_head.index("任务")
-index_intent = file_head.index("意图")
-index_nlu = file_head.index("语义")
-index_command = file_head.index("command")
-index_nlg = file_head.index("nlg")
-index_error_message = file_head.index("错误提示")
+index_skill = file_head.index("实际技能")
+index_task = file_head.index("实际任务")
+index_intent = file_head.index("实际意图")
+index_nlu = file_head.index("实际语义")
+index_command = file_head.index("实际command")
+index_nlg = file_head.index("实际nlg")
 index_reality = file_head.index("实际结果")
+
+str_split_line = "--------------------"
 
 
 # 格式化实际语义，换行显示
 def format_reality_nlu(nlu):
     reality_nlu = []
     for slot in nlu["semantics"]["request"]["slots"]:
-        if "intent" != slot["name"]:
+        if "intent" == slot["name"]:
+            reality_nlu.append(str_split_line)
+        else:
             reality_nlu.append(slot["name"] + "=" + slot["value"])
             if slot.get("rawvalue") is not None:
                 reality_nlu.append(slot["name"] + "_raw=" + slot["rawvalue"])
+    if len(reality_nlu) > 0 and str_split_line == reality_nlu[0]:
+        reality_nlu = reality_nlu[1:]
     return "\n".join(reality_nlu)
 
 
@@ -64,6 +70,18 @@ def format_reality_command(command):
     if command.get("param") is not None:
         for k, v in command["param"].items():
             reality_command.append("param." + k + "=" + v)
+    return "\n".join(reality_command)
+
+
+# 格式化实际inspire command，换行显示
+def format_reality_command_inspire(inspire):
+    reality_command = []
+    for ins in inspire:
+        if ins.get('command') is not None:
+            reality_command.append(str_split_line)
+            reality_command.append(format_reality_command(ins['command']))
+    if len(reality_command) > 0 and str_split_line == reality_command[0]:
+        reality_command = reality_command[1:]
     return "\n".join(reality_command)
 
 
@@ -88,13 +106,34 @@ async def textRequest(ws, refText, sessionId=None):
         print(exp)
 
 
+async def systemSetting(ws):
+    # 做技能级配置。
+    content = {
+        "topic": "system.settings",
+        "settings": [
+            {
+                "key": "filterSwitch",
+                "value": "off"
+            }
+        ]
+    }
+    try:
+        await ws.send(json.dumps(content))
+        resp = await ws.recv()
+        print(resp)
+    except websockets.WebSocketException as exp:
+        print(exp)
+
+
 # 执行webSocket请求，执行测试
 async def do_test():
     session_id = uuid4().hex
+    print("start time: %s" % time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
     # 打印webSocket连接地址
     print("webSocket连接地址：%s" % SERVER_URL)
     # 创建webSocket连接
-    async with websockets.connect(SERVER_URL) as websocket:
+    async with websockets.connect(SERVER_URL, ping_interval=None, ping_timeout=None) as websocket:
+        #         resp = await systemSetting(websocket)
         # 循环测试数据
         for datas in lines[1:]:
             # 获取测试用例
@@ -124,6 +163,8 @@ async def do_test():
                 datas[index_nlu] = format_reality_nlu(result["nlu"])
             if result["dm"].get("command") is not None:
                 datas[index_command] = format_reality_command(result["dm"]["command"])
+            elif result["dm"].get("inspire") is not None:
+                datas[index_command] = format_reality_command_inspire(result["dm"]["inspire"])
             if result["dm"].get("nlg") is not None:
                 datas[index_nlg] = result["dm"]["nlg"]
 
@@ -132,5 +173,6 @@ asyncio.get_event_loop().run_until_complete(do_test())
 
 # 回写数据结果到Excel文件
 df = pd.DataFrame(lines[1:], columns=file_head)
-df.to_excel("测试结果-" + file_name, sheet_name="%s-%s-%s" % (sheet_name, PRODUCT_ID, BRANCH), index=False, header=True)
+df.to_excel("测试结果-" + file_name, sheet_name="%s-%s" % (PRODUCT_ID, BRANCH), index=False, header=True)
+print("end time: %s" % time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 print("测试完成！")
