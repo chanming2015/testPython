434f38816d9a8a11ffbd129fea0f08f8d1d4369e
maosen.xu <maosen.xu@aispeech.com>
2023-04-19 15:31:56 +0800

    bug fix.

--- aispeech/ddsserver/dm/sync_skill_conf.py
+++ aispeech/ddsserver/dm/sync_skill_conf.py
@@ -12,9 +12,9 @@ import json
 import time
        
 if __name__ == '__main__':
-#     productIds = ['tryProductId', '279613689', '279606354', '279610639', '279610836', '279609887', '279610994', '279614157', '279615052', '279614268', '279614778']
+#     productIds = ['tryProductId', '279613689', '279606354', '279615447', '279616508']
 
-    productIds = ['tryProductId', '279606354', '279613689']
+    productIds = ['279606354']
     j = json.loads(open(u'C:/Users/Administrator/Desktop/长城车控技能配置.json', encoding='utf-8').read())
 
 #     productIds = ['279611361']
@@ -22,11 +22,19 @@ if __name__ == '__main__':
 # 
 #     productIds = ['279611322', '279613565']
 #     j = json.loads(open(u'C:/Users/Administrator/Desktop/北汽车控技能配置.json', encoding='utf-8').read())
-    
+
+#     productIds = ['279610994']
+#     j = json.loads(open(u'C:/Users/Administrator/Desktop/洛轲车控技能配置.json', encoding='utf-8').read())
+#     
     skillIds = ['2022011900000133']
     for p in productIds:
         for s in skillIds:
-            url = 'http://localhost:8080/v2/lyra/webhook/dsk/skill/dmconfig?productId=%s&skillId=%s' % (p, s)
+            url = 'http://apis.beta.dui.ai/v2/lyra/webhook/dsk/skill/dmconfig?productId=%s&skillId=%s' % (p, s)
             resp = requests.post(url, json=j)
             print(resp.status_code)
-            time.sleep(2)
+            time.sleep(1)
+#             
+#             url = 'http://apis.dui.ai/v2/lyra/webhook/dsk/skill/dmconfig/sync?productId=%s&skillId=%s' % (p, s)
+#             resp = requests.post(url)
+#             print(resp.status_code)
+#             time.sleep(1)
