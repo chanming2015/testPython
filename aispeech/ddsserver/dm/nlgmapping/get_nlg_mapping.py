32829b7784c6c1252f62490287c7480557b7aa9f
maosen.xu <maosen.xu@aispeech.com>
2024-07-03 12:08:15 +0800

    nlg_mapping bug fix.

--- aispeech/ddsserver/dm/nlgmapping/get_nlg_mapping.py
+++ aispeech/ddsserver/dm/nlgmapping/get_nlg_mapping.py
@@ -22,20 +22,24 @@ nlg_mappings = {}
 for sheet_name in excel_file.sheet_names:
     nlg_mapping = {}
     lines = pd.read_excel(file_name, sheet_name=sheet_name, header=None).values.tolist()
-    file_head = list(map(lambda x:x.lower().replace('-', '_') if type(x) == str else x, lines[0]))
     
+    if len(lines) < 2:
+        continue
+    file_head = list(map(lambda x:x.lower().replace('-', '_') if type(x) == str else x, lines[0]))
     if "nlg" not in file_head or "nlg_id" not in file_head:
         continue
+
 #     print(sheet_name)
     # 获取NLG和ID的数据
     index_nlg = file_head.index("nlg")
     index_nlg_id = file_head.index("nlg_id")
     
     for datas in lines[1:]:
-        # 判断同一个sheet中是否存在相同的nlg_id
-        if type(datas[index_nlg_id]) != str:
+        if type(datas[index_nlg]) != str or type(datas[index_nlg_id]) != str:
             continue
-        if nlg_mapping.get(datas[index_nlg_id]):
+        # 判断同一个sheet中是否存在相同的nlg_id
+        # 存在相同nlg_id，但nlg不同，输出错误提示
+        if nlg_mapping.get(datas[index_nlg_id]) and nlg_mapping.get(datas[index_nlg_id]) != datas[index_nlg]:
             print("sheet_name: %s 存在相同的nlg_id: %s" % (sheet_name, datas[index_nlg_id]))
 #             break
         nlg_mapping[datas[index_nlg_id]] = datas[index_nlg]
@@ -45,8 +49,8 @@ for sheet_name in excel_file.sheet_names:
     for sheet, nlg_mapping_sheet in nlg_mappings.items():
         if sheet != sheet_name:
             intersection = list(set(nlg_mapping.keys()) & set(nlg_mapping_sheet.keys()))
-            if len(intersection) > 0:
-                print("sheet_name: %s 、 %s 存在相同的nlg_id: %s" % (sheet, sheet_name, intersection))
-                break
+            for its in intersection:
+                if nlg_mappings[sheet][its] != nlg_mappings[sheet_name][its]:
+                    print("sheet_name: %s 、 %s 存在相同的nlg_id: %s" % (sheet, sheet_name, its))
 
 print(json.dumps(reduce(dict_merge, nlg_mappings.values()), ensure_ascii=False))
